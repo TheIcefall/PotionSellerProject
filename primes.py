cd3a4b198a46775fdcb29d58b2b2cd1e494fb719
thomastran2k2 <80449239+thomastran2k2@users.noreply.github.com>
2022-05-05 02:49:57 +1000

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,2 +1,15 @@
 def largest_prime(k: int) -> int:
-    raise NotImplementedError()
+    def largest_prime(k: int) -> int:
+    prime = [True for i in range(k)]
+    p = 2
+    while(p*p <= k - 1):
+        if (prime[p] == True):
+            for i in range(p**2, k, p):
+                prime[i] = False
+        p += 1
+    prime[0] = False
+    prime[1] = False
+    for p in range(k - 1, -1, - 1):
+        if prime[p] == True:
+            return p
+#print(largest_prime(10))
