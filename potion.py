a8dce6ee7d21c92d0f2f88cd77e3545fad350361
thomastran2k2 <80449239+thomastran2k2@users.noreply.github.com>
2022-05-05 03:22:42 +1000

    Update potion.py

--- potion.py
+++ potion.py
@@ -1,19 +1,32 @@
 class Potion:
     
     def __init__(self, potion_type: str, name: str, buy_price: float, quantity: float) -> None:
-        raise NotImplementedError()
+        self.potion_type = potion_type
+        self.name = name
+        self.buy_price = buy_price
+        self.quantity = quantity
+
 
     @classmethod
     def create_empty(cls, potion_type: str, name: str, buy_price: float) -> 'Potion':
-        """"""
-        raise NotImplementedError()
+        return cls(potion_type, name, buy_price, 0)
+
 
     @classmethod
     def good_hash(cls, potion_name: str, tablesize: int) -> int:
         """"""
-        raise NotImplementedError()
+        sum = 0
+        for i in range(len(potion_name)):
+            sum += ord(potion_name[i])*i
+        hash_value = sum//tablesize
+        return hash_value
 
     @classmethod
     def bad_hash(cls, potion_name: str, tablesize: int) -> int:
         """"""
-        raise NotImplementedError()
+        sum = 0
+        for i in range(len(potion_name)):
+            sum += ord(potion_name[i])
+        hash_value = sum//tablesize
+        return hash_value
+
