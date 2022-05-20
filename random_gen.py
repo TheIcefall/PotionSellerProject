722d8b0f69d48b8c44f37588f6a5eadc046a8d70
thomastran2k2 <80449239+thomastran2k2@users.noreply.github.com>
2022-05-20 14:09:09 +1000

    Update random_gen.py

--- random_gen.py
+++ random_gen.py
@@ -8,11 +8,32 @@ def lcg(modulus: int, a: int, c: int, seed: int) -> Generator[int, None, None]:
 
 class RandomGen:
     
-    def __init__(self) -> None:
-        raise NotImplementedError()
+    def __init__(self, seed: int = 0) -> None:
+        self.seed = seed
+
 
     def randint(self, k: int) -> int:
-        raise NotImplementedError()
+        num = []
+        res = ""
+        it = lcg(pow(2,32), 1347758132132, 1, self.seed)
+        it.__next__()
+        for i in range(5):
+            ran = it.__next__()
+            ran = ran >> 16
+            num.append(format(ran, '016b'))
+        for j in range(16):
+            count = 0
+            for h in range(5):
+                if num[h][j] == "1":
+                    count += 1
+            res += str(count)
+        res1 = ""
+        for l in res:
+            if int(l) >= 3:
+                res1 += "1"
+            else:
+                res1 += "0"
+        return int(res1, 2) % k + 1
 
 if __name__ == "__main__":
     Random_gen = lcg(pow(2,32), 134775813, 1, 0)
